0385f52739065a8473c8e2a9dfcdbf5fa3dbab6a
fabiodrneles <fabiodrneles@gmail.com>
2024-07-17 12:34:29 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import ButtonGradient from "./assets/svg/ButtonGradient"
+
+const App = () => {
+
+  return (
+    <>
+      <h1 class="text-3xl font-bold underline"> Hello world!</h1>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Button className="mt-10" href="#login">
+          Something
+        </Button>
+
+      </div>
+      <ButtonGradient />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Button.jsx
@@ -0,0 +1,12 @@
+import ButtonSvg from "../assets/svg/ButtonSvg";
+
+const Button = ({ className, href, onClick, children, px, white }) => {
+    const renderButton = () => (
+        <Button>
+            <span>{children}</span>
+            {ButtonSvg(white)}
+        </Button>
+    )
+}
+
+export default Button
