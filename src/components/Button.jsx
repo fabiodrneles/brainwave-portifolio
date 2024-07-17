6a7460b2e5d1a16ff50b8a47929f4c9f49be1275
fabiodrneles <fabiodrneles@gmail.com>
2024-07-17 12:35:26 -0300

    teste

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,6 +3,7 @@ import ButtonSvg from "../assets/svg/ButtonSvg";
 const Button = ({ className, href, onClick, children, px, white }) => {
     const renderButton = () => (
         <Button>
+            
             <span>{children}</span>
             {ButtonSvg(white)}
         </Button>
